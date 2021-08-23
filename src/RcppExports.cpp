96f4997d42f20e1e3e1a5521c095c892f160d8db
benjaminleroy <benjaminleroyusa@gmail.com>
2021-08-23 10:06:33 -0400

    minor change to cpp exports

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // crossdist
 NumericMatrix crossdist(NumericMatrix m1, NumericMatrix m2);
 RcppExport SEXP _simulationBands_crossdist(SEXP m1SEXP, SEXP m2SEXP) {
